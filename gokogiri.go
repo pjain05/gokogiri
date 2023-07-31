07a8ab012febf79132a892d1c83067d8f133116a
Puneet Jain <105224306+pjain05@users.noreply.github.com>
2023-07-31 13:46:22 +0530

    Update gokogiri.go

--- gokogiri.go
+++ gokogiri.go
@@ -8,8 +8,8 @@ and a simple DOM-like inteface allows for building up documents from scratch.
 package gokogiri
 
 import (
-	"github.com/moovweb/gokogiri/html"
-	"github.com/moovweb/gokogiri/xml"
+	"github.com/pjain05/gokogiri/html"
+	"github.com/pjain05/gokogiri/xml"
 )
 
 /*
