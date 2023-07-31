804bb25e72c8b271b14ebfa06f78f4ba2e52391a
Puneet Jain <105224306+pjain05@users.noreply.github.com>
2023-07-31 13:45:10 +0530

    Update utils_test.go

--- xml/utils_test.go
+++ xml/utils_test.go
@@ -3,8 +3,8 @@ package xml
 import (
 	"errors"
 	"fmt"
-	"github.com/moovweb/gokogiri/help"
-	"github.com/moovweb/gokogiri/xpath"
+	"github.com/pjain05/gokogiri/help"
+	"github.com/pjain05/gokogiri/xpath"
 	"io/ioutil"
 	"path/filepath"
 	"strings"
